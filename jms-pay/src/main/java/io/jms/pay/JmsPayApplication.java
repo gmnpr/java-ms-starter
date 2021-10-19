0bec2a5777a48dc890c0573fdd73cf4f49dde05b
rkorobeynikov <Rostislav.Korobeynikov@dpd.ru>
2021-10-19 21:13:59 +0300

    Added pay service

---
+++ jms-pay/src/main/java/io/jms/pay/JmsPayApplication.java
@@ -0,0 +1,12 @@
+package io.jms.pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmsPayApplication.class, args);
+    }
+}
---
+++ jms-pay/src/test/java/io/jms/pay/JmsPayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jms.pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
