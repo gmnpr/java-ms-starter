ead9f5f364e40af5b247af23134f5554826dbfc7
rkorobeynikov <Rostislav.Korobeynikov@dpd.ru>
2021-10-19 20:36:59 +0300

    Init gateway service

---
+++ jms-gateway/src/main/java/io/jms/gateway/JmsGatewayApplication.java
@@ -0,0 +1,12 @@
+package io.jms.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmsGatewayApplication.class, args);
+    }
+}
---
+++ jms-gateway/src/test/java/io/jms/gateway/JmsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jms.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
